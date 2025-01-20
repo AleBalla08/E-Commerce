b7bcc85d2c02edd99db6e918f90517595c4357aa
Alexandre Ballardin <aleballa08@gmail.com>
2025-01-20 17:52:52 -0300

    first one

---
+++ script.js
@@ -0,0 +1,48 @@
+const url = 'https://fakestoreapi.com/products'
+
+const container = document.querySelector('#product1 .pro-container');
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao requisitar')
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data, 'dados da api')
+        data.forEach(product => {
+            const pro = document.createElement('div')
+            pro.classList.add('pro')
+
+            pro.innerHTML = `
+                <img src="${product.image}" alt="">
+                <div class="desc">
+                    <span>${product.category}</span>
+                    <h5>${product.title}</h5>
+                    <div class="stars">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <h4>$${product.price}</h4>
+                </div>
+                <a href="#"><i class="fa fa-cart-shopping cart"></i></a>`
+            
+
+            container.appendChild(pro)
+            
+        });
+    })
+    .catch(error => {
+        console.log(error, 'erro')
+    })
+
+
+
+
+
+
+
